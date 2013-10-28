c3694485e472bcfbdb1d2ff4185dd54b1638285d
Roberto D'Auria <evfirerob@gmail.com>
2013-10-28 10:13:35 +0100

    Add datatypes.c

---
+++ c/datatypes.c
@@ -0,0 +1,14 @@
+/* Author: Roberto D'Auria
+ *
+ * Prints the size of commonly used data types
+ */
+
+#include <stdio.h>
+
+int main(){
+    printf("sizeof(char)  -> %d\n", sizeof(char));
+    printf("sizeof(int)   -> %d\n", sizeof(int));
+    printf("sizeof(float) -> %d\n", sizeof(float));
+    printf("sizeof(long)  -> %d\n", sizeof(long));
+    return 0;
+}
